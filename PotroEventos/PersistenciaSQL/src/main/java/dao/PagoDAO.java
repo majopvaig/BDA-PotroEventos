61b28c0a7f90054b9bdb509523042ce656578883
marronxd <aaron.burciaga262788@potros.itson.edu.mx>
2026-05-22 17:21:39 -0700

    agregue un metodo de pago y reembolso

--- PotroEventos/PersistenciaSQL/src/main/java/dao/PagoDAO.java
+++ PotroEventos/PersistenciaSQL/src/main/java/dao/PagoDAO.java
@@ -1,4 +1,66 @@
 package dao;
 
-public class PagoDAO {
+import Entitys.Pago;
+import adapters.IdAdapter;
+import conexion.ConexionBD;
+import excepciones.PersistenciaException;
+import interfaces.IPagoDAO;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class PagoDAO implements IPagoDAO {
+
+    private static PagoDAO instance;
+
+    private PagoDAO() {
+
+    }
+    public static PagoDAO getInstance(){
+        if(instance == null){
+            instance = new PagoDAO();
+        }
+        return instance;
+    }
+
+    @Override
+    public Pago agregarPago(Pago pago, String idReservacion) throws PersistenciaException {
+        if(pago == null){
+            throw new PersistenciaException("No hay pago por registrar.");
+        }
+        if(idReservacion == null){
+            throw new PersistenciaException("No hay puede asociarse con una reserva.");
+        }
+        //  --- sentencia ---
+        String sql =
+                """
+                insert into pagos 
+                (fecha_operacion, importe, metodoPago, idReservacion)
+                values(?, ?, ?, ?)
+                """;
+        //  --- conexion ---
+        try(Connection con = ConexionBD.crearConexion();
+            PreparedStatement ps = con.prepareStatement(sql)){
+            //  --- setear valores ---
+            ps.setDate(1, Date.valueOf(pago.getFechaOperacion().toLocalDate()));
+            ps.setDouble(2, pago.getImporte());
+            ps.setString(3, pago.getMetodoPago());
+            ps.setLong(4, IdAdapter.stringALong(idReservacion));
+            //  --- ejecutar ---
+            if(ps.executeUpdate() <= 0){
+                return null;
+            }
+            return pago;
+        }catch (SQLException e){
+            throw new PersistenciaException("Error al establecer conexión.");
+        }
+    }
+
+    @Override
+    public Pago consultarReservacion(String idPago) throws PersistenciaException {
+        
+        return null;
+    }
 }
---
+++ PotroEventos/PersistenciaSQL/src/main/java/dao/ReembolsoDAO.java
@@ -0,0 +1,63 @@
+package dao;
+
+import Entitys.Reembolso;
+import adapters.IdAdapter;
+import conexion.ConexionBD;
+import excepciones.PersistenciaException;
+import interfaces.IReembolsoDAO;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ReembolsoDAO implements IReembolsoDAO {
+
+    //  --- instancia ---
+    private static ReembolsoDAO instance;
+    private ReembolsoDAO() {
+    }
+    //  --- singleton ---
+    public static ReembolsoDAO getInstance(){
+        if(instance == null){
+            instance = new ReembolsoDAO();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public Reembolso agregarReembolso(Reembolso reembolso, String idDevolucion) throws PersistenciaException {
+
+        String sql =
+                """
+                insert into reembolsos
+                (id_operacion, fechaOperacion, importe, metodoPago)
+                values(?, ?, ?, ?)
+                """;
+        //  --- conexion ---
+        try(Connection con = ConexionBD.crearConexion();
+            PreparedStatement ps = con.prepareStatement(sql)){
+
+            //  --- setear valores ---
+            Long idOperacion = IdAdapter.stringALong(reembolso.getIdOperacion());
+            ps.setLong(1, idOperacion);
+            ps.setDate(2, Date.valueOf(reembolso.getFechaOperacion().toLocalDate()));
+            ps.setDouble(3, reembolso.getImporte());
+            ps.setString(4, reembolso.getMetodoPago());
+
+            //  --- ejecutar ---
+            if(ps.executeUpdate() <= 0){
+                return null;
+            }
+            return reembolso;
+        }catch (SQLException ex){
+            throw new PersistenciaException("Error de conexion.");
+        }
+    }
+
+    @Override
+    public Reembolso consultarReembolso(String idReembolso) throws PersistenciaException {
+        return null;
+    }
+}
